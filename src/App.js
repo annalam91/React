2b431dbd0068d200ecd53ea956a270745f9cfec4
Anna Lam <annalam91@gmail.com>
2020-06-23 16:18:28 +0100

    Changing of state working demo

--- src/App.js
+++ src/App.js
@@ -5,35 +5,16 @@ import todosData from "./api/todosData";
 
 class App extends React.Component {
   constructor() {
-      super()
-      this.state = {
-          todos: todosData
-      }
-      this.handleChange = this.handleChange.bind(this)
+    super()
   }
-  
-  handleChange(id) {
-      this.setState(prevState => {
-          const updatedTodos = prevState.todos.map(todo => {
-              if(todo.id === id) {
-                  todo.completed = !todo.completed
-              }
-              return todo
-          })
-          return {
-              todos:updatedTodos
-          }
-      })
-  }
-  
   render() {
-      const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
-      
-      return (
-          <div className="todo-list">
-              {todoItems}
-          </div>
-      )    
+  const productComponents = productData.map(thing => <Product key={thing.id} product={thing}/>)
+  
+  return (
+      <div>
+          {productComponents}
+      </div>
+  )
   }
 }
 
