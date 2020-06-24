dfa4f4aec3e24af666954db68f6b7d30e672afb8
Anna Lam <anna.lam@cgi.com>
2020-06-24 18:31:07 +0100

    Separated into smart and dumb components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles/App.css";
+import FormComponent from './components/FormComponent';
 
 class App extends React.Component {
   constructor() {
@@ -9,7 +10,7 @@ class App extends React.Component {
       lastName: "",
       age: "",
       gender: "",
-      destination: "California",
+      destination: "",
       vegetarian: false,
       dairyFree: false,
       nutFree: false,
@@ -24,97 +25,11 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div className="Main">
-        <main>
-          <form>
-            <input 
-              type="text"
-              value={this.state.firstName} 
-              placeholder="First name"
-              onChange={this.handleChange}
-              name="firstName"
-               /> <br/>
-            <input
-              type="text"
-              value={this.state.lastName} 
-              placeholder="Last name"
-              onChange={this.handleChange}
-              name="lastName"
-              /> <br/>
-            <input
-              type="number"
-              value={this.state.age} 
-              placeholder="Age"
-              onChange={this.handleChange}
-              name="age"
-              /> <br/>
-              <label>
-            <input
-              type="radio"
-              value="female"
-              checked={this.state.gender === "female"}
-              onChange={this.handleChange}
-              name="gender"
-              /> Female
-              </label> <br/>
-              <label> 
-               <input
-              type="radio"
-              value="male"
-              checked={this.state.gender === "male"}
-              onChange={this.handleChange}
-              name="gender"
-              />Male
-              </label> <br/>
-              <label>Select your destination: 
-                <select value={this.state.destination} onChange={this.handleChange} name="destination">
-                  <option value="California">California</option>
-                  <option value="Hawaii">Hawaii</option>
-                  <option value="Chicago">Chicago</option>
-                </select>
-              </label> <br/> 
-              <label>Dietary restrictions
-              <input
-              type="checkbox"
-              value={this.state.vegetarian}
-              checked={this.state.vegetarian}
-              onChange={this.handleChange}
-              name="vegetarian"
-              />Vegeterian
-              </label>
-              <label>
-              <input
-              type="checkbox"
-              value={this.state.dairyFree}
-              checked={this.state.dairyFree}
-              onChange={this.handleChange}
-              name="dairyFree"
-              />Dairy Free
-              </label> 
-              <label>
-              <input
-              type="checkbox"
-              value={this.state.nutFree}
-              checked={this.state.nutFree}
-              onChange={this.handleChange}
-              name="nutFree"
-              />Nut Free
-              </label> 
-              <label>
-              <input
-              type="checkbox"
-              value={this.state.glutenFree}
-              checked={this.state.glutenFree}
-              onChange={this.handleChange}
-              name="glutenFree"
-              />Gluten Free
-              </label> <br />
-              <button>Submit</button>
-          </form>
-        </main>
-      </div>
-    );
+    return(
+   <FormComponent
+   handleChange={this.handleChange}
+   data={this.state} />
+    )
   }
 }
 
