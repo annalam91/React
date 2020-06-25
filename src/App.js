0e3587d2c2e05ee476b0bdd37f141802ff13c5f5
Anna Lam <anna.lam@cgi.com>
2020-06-25 11:58:27 +0100

    useState demo working

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import React from "react";
+import React, {useState}  from "react";
 import "./styles/App.css";
-import FormComponent from './components/FormComponent';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      firstName: "",
-      lastName: "",
-      age: "",
-      gender: "",
-      destination: "",
-      vegetarian: false,
-      dairyFree: false,
-      nutFree: false,
-      glutenFree: false
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+function App() {
+  const value = useState("Yes")
+  console.log(value)
 
-  handleChange(event){
-    const {name, value, type, checked} = event.target
-    type === "checkbox" ? this.setState({[name] : checked}) : this.setState({ [name]:value})
-  }
-
-  render() {
-    return(
-   <FormComponent
-   handleChange={this.handleChange}
-   data={this.state} />
-    )
-  }
+  return(
+    <div>
+      <h3>Does this useState work? {value[0]}</h3>
+    </div>
+  )
 }
-
 export default App;
