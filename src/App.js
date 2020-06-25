4427dcec6ffb29105c67c292eacb86c7a347e07e
Anna Lam <anna.lam@cgi.com>
2020-06-25 12:01:02 +0100

    Example of array destructuring - demo works

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, {useState}  from "react";
 import "./styles/App.css";
 
 function App() {
-  const value = useState("Yes")
+  const [ value ] = useState("Yes")
   console.log(value)
 
   return(
     <div>
-      <h3>Does this useState work? {value[0]}</h3>
+      <h3>Does this useState work? {value}</h3>
     </div>
   )
 }
