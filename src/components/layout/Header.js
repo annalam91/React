304952b84516f1426cf031586009116b5f6c3daf
Anna Lam <annalam91@gmail.com>
2020-06-30 15:35:25 +0100

    AppComplete

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,21 +1,46 @@
-import React from 'react';
-import {FaRegLightbulb} from 'react-icons/fa'
+import React, {useState} from 'react';
+import {FaRegLightbulb, FaLightbulb} from 'react-icons/fa'
+import {AddTask} from '../AddTask';
 
-export const Header = () => {
-    
-    return(
+export const Header = ({darkMode, setDarkMode}) => {
+
+    const [shouldShowMain, setShouldShowMain] = useState(false);
+    const [showQuickAddTask, setshowQuickAddTask] = useState(false);
+
+    let lightsOn;
+    if (darkMode) {
+        lightsOn = <FaRegLightbulb/>
+    } else {
+        lightsOn = <FaLightbulb/>
+    }
+
+    return (
         <header className="header">
             <nav>
                 <div className="logo">
-                    <img src="/images/logo.png" alt="Recipe" />
+                    <img src="/images/logo.png" alt="Recipe"/>
                 </div>
                 <div className="settings">
                     <ul>
-                        <li className="settings__add">+</li>
-                        <li className="settings__darkmode "><FaRegLightbulb /></li>
+                        <li className="settings__add"
+                            onClick={
+                                () => {
+                                    setshowQuickAddTask(true);
+                                    setShouldShowMain(true)
+                                }
+                        }>+</li>
+                        <li className="settings__darkmode"
+                            onClick={
+                                () => setDarkMode(!darkMode)
+                        }>
+                            {lightsOn}</li>
                     </ul>
                 </div>
             </nav>
+            <AddTask showAddTaskMain ={false}
+                shouldShowMain={shouldShowMain}
+                showQuickAddTask={showQuickAddTask}
+                setShowQuickAddTask={setshowQuickAddTask}/>
         </header>
     )
 }
