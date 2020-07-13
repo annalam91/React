43ea3a31966adbcc6630d341f5a2ffd16904a744
Anna Lam <anna.lam@cgi.com>
2020-07-13 09:13:17 +0100

    useEffect with working examples

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 import "./styles/App.css";
 import Counter from "./components/Counter";
+import CounterUseEffect from "./components/CounterUseEffect";
+import MouseEventUseEffect from "./components/MouseEventUseEffect";
+import IntervalCounter from "./components/IntervalCounter";
+import DataFetching from "./components/DataFetching";
 
 function App() {
-  const [hidden, setHidden] = useState(true);
+  const [selected, setSelected] = useState();
+
+  function handleSelect(e){
+    setSelected(e.target.id);
+  }
 
   return (
     <div>
-      <button onClick={() => setHidden(!hidden)}>useState example:</button>
-      {!hidden ? <Counter /> : null}
+      <div>
+        <button id="1" onClick={handleSelect}>useState example:</button>
+        {selected === "1" && <Counter />}
+        <br />
+      </div>
+      <div>
+        <button id="2" onClick={handleSelect}>useEffect example:</button>
+        {selected === "2" && <CounterUseEffect />} <br />
+        {selected === "2" && <MouseEventUseEffect />}<br />
+        {selected === "2" && <IntervalCounter />}<br />
+        {selected === "2" && <DataFetching />}<br />
+      </div>
     </div>
   );
 }
