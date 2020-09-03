63ed492a5d097f8f381a606727517f6d59e13817
Anna Lam <annalam91@gmail.com>
2020-09-03 18:07:37 +0100

    Basic example of mern

--- src/App.js
+++ src/App.js
@@ -1,154 +1,60 @@
-import React, {useState, useReducer} from "react";
-import "./styles/App.css";
-import Counter from "./components/Counter";
-import CounterUseEffect from "./components/CounterUseEffect";
-import MouseEventUseEffect from "./components/MouseEventUseEffect";
-import IntervalCounter from "./components/IntervalCounter";
-import DataFetching from "./components/DataFetching";
-import ContextHooks from "./components/ContextHooks";
-import ReducerCounter from "./components/ReducerCounter";
-import ReducerComplexCounter from "./components/ReducerComplexCounter";
-import MultipleUseReducer from "./components/MultipleUseReducer";
-import UseReducerUseContextExample from "./components/UseReducerUseContextExample";
-import UseReducerUseContextAnother from "./components/UseReducerUseContextAnother";
-import DataFetchingUsingReducer from "./components/DataFetchingUsingReducer";
-import Comparsion from "./components/Comparsion";
-import ParentComponent from "./components/useCallbackExample/ParentComponent";
-import CounterUseMemo from './components/CounterUseMemo';
-import FocusInput from './components/FocusInput';
-import HookTimer from './components/HookTimer';
+import React, { Component } from 'react';
+import './styles/App.css';
 
-export const UserContext = React.createContext();
-export const TutorialContext = React.createContext();
-export const CountContext = React.createContext();
-
-const intitalState = 0;
-const reducer = (currentState, action) => {
-    switch (action) {
-        case "increment":
-            return currentState + 1;
-        case "decrement":
-            return currentState - 1;
-        case "reset":
-            return intitalState;
-        default:
-            return currentState;
-    }
-};
-
-function App() {
-    const [count, dispatch] = useReducer(reducer, intitalState);
-    const [selected, setSelected] = useState();
-
-    const handleSelect = (e) => {
-        setSelected(e.target.id);
-    };
-
+class App extends Component {
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
+  
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+  
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    
+    return body;
+  };
+  
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+    
+    this.setState({ responseToPost: body });
+  };
+  
+render() {
     return (
-        <div>
-            <button id="comparsion"
-                onClick={handleSelect}>
-                Comparsion - When to use:
-            </button>
-            {
-            selected === "comparsion" && <Comparsion/>
-        }
-            <br/>
-            <button id="useState"
-                onClick={handleSelect}>
-                useState example:
-            </button>
-            {
-            selected === "useState" && <Counter/>
-        }
-            <br/>
-            <button id="useEffect"
-                onClick={handleSelect}>
-                useEffect example:
-            </button>
-            {
-            selected === "useEffect" && <CounterUseEffect/>
-        }
-            {
-            selected === "useEffect" && <MouseEventUseEffect/>
-        }
-            {
-            selected === "useEffect" && <IntervalCounter/>
-        }
-            {
-            selected === "useEffect" && <DataFetching/>
-        }
-            <br/>
-            <button id="useContext"
-                onClick={handleSelect}>
-                useContext example:
-            </button>
-            <UserContext.Provider value={"Username: Anna"}>
-                <TutorialContext.Provider value={"React Hooks tutorials"}>
-                    {
-                    selected === "useContext" && <ContextHooks/>
-                }
-                    <br/>
-                </TutorialContext.Provider>
-            </UserContext.Provider>
-            <button id="useReducer"
-                onClick={handleSelect}>
-                useReducer example:
-            </button>
-            {
-            selected === "useReducer" && <ReducerCounter/>
-        }
-            {
-            selected === "useReducer" && <ReducerComplexCounter/>
-        }
-            {
-            selected === "useReducer" && <MultipleUseReducer/>
-        }
-            <CountContext.Provider value={
-                {
-                    countState: count,
-                    countDispatch: dispatch
-                }
-            }>
-                {
-                selected === "useReducer" && <b>App.js - Count - {count}</b>
-            }
-                {
-                selected === "useReducer" && <UseReducerUseContextExample/>
-            }
-                {
-                selected === "useReducer" && <UseReducerUseContextAnother/>
-            } </CountContext.Provider>
-            {
-            selected === "useReducer" && <DataFetchingUsingReducer/>
-        }
-            <br/>
-            <button id="useCallback"
-                onClick={handleSelect}>
-                useCallback example:
-            </button>
-            {
-            selected === "useCallback" && <ParentComponent/>
-        }
-            <br/>
-            <button id="useMemo"
-                onClick={handleSelect}>
-                useMemo example:
-            </button>
-            {
-            selected === "useMemo" && <CounterUseMemo/>
-        }
-            <br/>
-            <button id="useRef"
-                onClick={handleSelect}>
-                useRef example:
-            </button>
-            {
-            selected === "useRef" && <FocusInput/>
-        }
-            {
-            selected === "useRef" && <HookTimer/>
-        } </div>
+      <div className="App">
+        <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
+      </div>
     );
+  }
 }
+
 export default App;
